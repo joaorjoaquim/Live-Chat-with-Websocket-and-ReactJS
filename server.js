e08a30204eb82b83ac9188ca2997fced36eade91
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-09-30 14:27:07 -0300

    server, initial html and css

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+server.listen(PORT);
