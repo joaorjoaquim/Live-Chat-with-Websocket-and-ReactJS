a8c831b0aa0374445f657f4d04313076eee8df29
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-09-30 16:25:43 -0300

    look out the emit problem

--- server.js
+++ server.js
@@ -15,4 +15,12 @@ app.use('/', (req, res) => {
     res.render('index.html');
 });
 
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.on('sendMessage', data => {
+        console.log(data);
+    })
+});
+
 server.listen(PORT);
